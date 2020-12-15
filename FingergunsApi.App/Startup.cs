81bd34e40ca0e7bb55aad2cd1529e03196094d22
Erik Jenkins <ejenkins132@gmail.com>
2020-12-15 18:55:18 -0500

    remove endpoint logging

--- FingergunsApi.App/Startup.cs
+++ FingergunsApi.App/Startup.cs
@@ -33,7 +33,7 @@ namespace FingergunsApi.App
             });
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "FingergunsApi.App", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FingergunsApi.App", Version = "v1" });
             });
         }
 
@@ -56,37 +56,6 @@ namespace FingergunsApi.App
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapGet("/", async context =>
-                {
-                    context.Response.ContentType = "text/plain";
-
-                    // Host info
-                    var name = Dns.GetHostName(); // get container id
-                    var ip = Dns.GetHostEntry(name).AddressList
-                        .FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
-                    Console.WriteLine($"Host Name: {Environment.MachineName} \t {name}\t {ip}");
-                    await context.Response.WriteAsync($"Host Name: {Environment.MachineName}{Environment.NewLine}");
-                    await context.Response.WriteAsync(Environment.NewLine);
-
-                    // Request method, scheme, and path
-                    await context.Response.WriteAsync($"Request Method: {context.Request.Method}{Environment.NewLine}");
-                    await context.Response.WriteAsync($"Request Scheme: {context.Request.Scheme}{Environment.NewLine}");
-                    await context.Response.WriteAsync(
-                        $"Request URL: {context.Request.GetDisplayUrl()}{Environment.NewLine}");
-                    await context.Response.WriteAsync($"Request Path: {context.Request.Path}{Environment.NewLine}");
-
-                    // Headers
-                    await context.Response.WriteAsync($"Request Headers:{Environment.NewLine}");
-                    foreach (var (key, value) in context.Request.Headers)
-                    {
-                        await context.Response.WriteAsync($"\t {key}: {value}{Environment.NewLine}");
-                    }
-
-                    await context.Response.WriteAsync(Environment.NewLine);
-
-                    // Connection: RemoteIp
-                    await context.Response.WriteAsync($"Request Remote IP: {context.Connection.RemoteIpAddress}");
-                });
             });
         }
     }
